5b93452138403e19c2289d3a35f8c7a4ae7c6df7
Alex Farrill <alex.farrill@gmail.com>
2011-12-16 18:30:44 -0600

    better

--- lib/admin_assistant/request.rb
+++ lib/admin_assistant/request.rb
@@ -48,9 +48,9 @@ class AdminAssistant
         params = {}
         split_params = {}
         whole_params = {}
-        @controller.params[:record].each do |k, v|
+        Array(@controller.params[:record]).each do |k, v|
           k =~ /\([0-9]+i\)$/ ? (split_params[k] = v) : (whole_params[k] = v)
-        end if @controller.params[:record]
+        end
         bases = split_params.map{ |k, v| k.gsub(/\([0-9]+i\)$/, '') }.uniq
         bases.each do |b|
           h = {}
